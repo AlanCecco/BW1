e3e8afd1bc600a84dc11e6e0ecea9c9a8a102459
AlanCecco <146008011+AlanCecco@users.noreply.github.com>
2023-10-19 17:17:44 +0200

    Stelle

--- feedback.js
+++ feedback.js
@@ -27,3 +27,12 @@
 // }
 
 // executeRating(ratingStars, ratingResult);
+
+var starRatings = document.querySelectorAll('.star-rating');
+ for (var index = 0; index < starRatings.length; index++) {
+     var starRating   = starRatings[index],
+         fullStars    = starRating.querySelector('.full-stars'),
+         rating       = parseFloat(starRating.dataset.rating) || 0,
+         percentWidth = rating * 20;
+  
+ }
